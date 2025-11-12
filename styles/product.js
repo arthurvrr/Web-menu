9ce34b1f4a2cf89f639888ff88ce831139868ff5
Arthur Rigo <arthurvrr@hotmail.com>
2025-11-11 22:37:21 -0300

    Update font paths, add product.js, fix script src
    Updated font-face src paths in global.css to use absolute URLs. Added styles/product.js with initial menu data structure. Fixed script source path in web-menu.html to use an absolute path.

---
+++ styles/product.js
@@ -0,0 +1,28 @@
+
+
+
+
+//Menu é um vetor dinâmico que representa os produtos,
+const menu = [
+
+    {
+        id: "risoles",
+        name: "Risoles",
+        basePrice: 1.60,
+        image:"",
+        type: "simple-flavor",
+
+        flavors: ["Fragno", ]
+
+
+
+    }
+
+
+
+
+
+
+
+];
+
