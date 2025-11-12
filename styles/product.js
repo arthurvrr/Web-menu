8ed0c32c66746bf911208120d3c0babb38de55b7
Arthur Rigo <arthurvrr@hotmail.com>
2025-11-12 13:33:35 -0300

    Prodcuts array

--- styles/product.js
+++ styles/product.js
@@ -5,19 +5,203 @@
 //Menu é um vetor dinâmico que representa os produtos,
 const menu = [
 
+/*========================================
+                SALGADOS
+ ========================================*/
+    // DATA-INTERACTION = "STATIC-PRODUCT"
     {
-        id: "risoles",
-        name: "Risoles",
+        id: "bolinha-queijo",
+        name: "BOLINHA DE QUEIJO",
+        category: "salgados",
+        basePrice: 1.80,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+
+    {
+        id: "coxinha-frango",
+        name: "COXINHA",
+        category: "salgados",
         basePrice: 1.60,
         image:"",
-        type: "simple-flavor",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
 
-        flavors: ["Fragno", ]
+
+    {
+        id: "croquete-carne",
+        name: "CROQUETE CARNE",
+        category: "salgados",
+        basePrice: 1.80,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
 
 
+    {
+        id: "enroladinho-frito",
+        name: "ENROLADINHO FRITO",
+        category: "salgados",
+        basePrice: 1.60,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+        
+    },
 
-    }
 
+    {
+        id: "folhadinho-salsicha",
+        name: "FOLHADINHO DE SALSICHA",
+        category: "salgados",
+        basePrice: 1.80,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+
+    },
+
+
+    {
+        id: "doguinho-salsicha",
+        name: "DOGUINHO",
+        category: "salgados",
+        basePrice: 1.80,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+
+    {
+        id: "prensadinho",
+        name: "PRENSADINHO",
+        category: "salgados",
+        basePrice: 2.60,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 40 gramas"
+    },
+
+
+    {
+        id: "cachorro-quente",
+        name: "CACHORRO QUENTE",
+        category: "salgados",
+        basePrice: 2.50,
+        image:"",
+        type: "static-product",
+        description: "Pãozinho fofinho com suculento recheio de salsicha picada ao molho de tomate, temperado com alho, cebola e um toque de salsa. Aprox. 40g por unidade."
+    },
+
+    /*========================================
+                    DOCES
+    ========================================*/
+    // DATA-INTERACTION = "STATIC-PRODUCT"
+    {
+        id: "brigadeiro-preto",
+        name: "BRIGADEIRO",
+        category: "doces",
+        basePrice: 1.70,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+    {
+        id: "brigadeiro-branco",
+        name: "BRANQUINHO",
+        category: "doces",
+        basePrice: 1.70,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+
+    {
+        id: "beijinho",
+        name: "BEIJINHO",
+        category: "doces",
+        basePrice: 1.70,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+
+    {
+        id: "docinho-amendoim",
+        name: "AMENDOIM",
+        category: "doces",
+        basePrice: 1.70,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+
+    {
+        id: "doce-de-leite",
+        name: "DOCE DE LEITE",
+        category: "doces",
+        basePrice: 1.70,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+
+    {
+        id: "docinho-nozes",
+        name: "NOZES",
+        category: "doces",
+        basePrice: 2.00,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+
+    {
+        id: "olho-sogra",
+        name: "OLHO DE SOGRA",
+        category: "doces",
+        basePrice: 2.00,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+
+    {
+        id: "ninho-nutella",
+        name: "NINHO & NUTELLO",
+        category: "doces",
+        basePrice: 2.20,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+
+    {
+        id: "camafeu-nozes",
+        name: "CAMAFEU",
+        category: "doces",
+        basePrice: 2.90,
+        image:"",
+        type: "static-product",
+        description: "Aproximadamente 20 gramas"
+    },
+
+    
+
+    
 
 
 
